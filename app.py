868308657bea80253438eda289422d39a71abb38
Irit Katriel <iritkatriel@yahoo.com>
2023-11-09 11:23:47 +0000

    simplify

--- app.py
+++ app.py
@@ -7,35 +7,24 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tokenize
 
-
 class Stage(ttk.Frame):
-    def __init__(self, title, refresh_action=None, editable=False,
-                 master=None):
+    def __init__(self, title, master=None):
         super().__init__(master)
         self.title = title
-        self.refresh_action = refresh_action
 
         tk.Label(self, text=title).grid(row=0,column=0, padx=5, pady=5)
+        self.init_layout()
+
+    def init_layout(self):
         self.text = tk.Text(self)
         self.text.grid(row=1,column=0, padx=5, pady=5)
-        if editable:
-            bottom_widget = ttk.Button(text="Refresh",
-                                       command=self.do_refresh,
-                                       master=self)
-        else:
-            bottom_widget = ttk.Label(text="", master=self)
 
-        bottom_widget.grid(row=2, column=0, padx=5, pady=5)
-
-    def do_refresh(self):
-        if self.refresh_action is not None:
-            text = self.text.get(1.0, "end-1c")
-            self.refresh_action(text)
+    def getvalue(self):
+        return self.text.get(1.0, "end-1c")
 
     def replace_text(self, value):
         self.text.delete(1.0, "end-1c")
         self.text.insert(tk.INSERT, value or "")
-        self.do_refresh()
 
 
 class App(tk.Tk):
@@ -52,35 +41,55 @@ class App(tk.Tk):
         self.controls.grid(row=1, column=0)
 
         self.source = Stage('Source',
-                            refresh_action=self.source_refreshed,
-                            editable=True,
                             master=self.displays)
-        self.tokens = Stage('Tokens', master=self.displays)
-        self.ast = Stage('AST', master=self.displays)
-        self.opt_ast = Stage('Optimized AST', master=self.displays)
+        self.tokens = Stage('Tokens',
+                            master=self.displays)
+        self.ast = Stage('AST',
+                         master=self.displays)
+        self.opt_ast = Stage('Optimized AST',
+                             master=self.displays)
 
         self.source.grid(row=0, column=0, padx=10, pady=5)
         self.tokens.grid(row=0, column=1, padx=10, pady=5)
         self.ast.grid(row=1, column=0, padx=10, pady=5)
         self.opt_ast.grid(row=1, column=1, padx=10, pady=5)
 
-        self.source.replace_text(self.DEFAULT_SOURCE)
+        ttk.Button(text="refresh",
+                   command=self.refresh,
+                   master=self.controls).grid(row=0, column=0)
         ttk.Button(text="close",
                    command=self.close,
-                   master=self.controls).grid(row=0, column=0)
+                   master=self.controls).grid(row=0, column=1)
 
-    def source_refreshed(self, src):
-        def pretty(input):
-            stream = io.StringIO()
-            pprint(input, stream=stream)
-            return stream.getvalue()
+        self.source.replace_text(self.DEFAULT_SOURCE)
+        self.refresh()
 
+    @staticmethod
+    def _pretty(input):
+        stream = io.StringIO()
+        pprint(input, stream=stream)
+        return stream.getvalue()
+
+    def refresh(self):
+        self.refresh_optimized_ast()
+        self.refresh_ast()
+        self.refresh_tokens()
+
+    def refresh_tokens(self):
+        src = self.source.getvalue()
         tokens = list(tokenize.tokenize(
                      io.BytesIO(src.encode('utf-8')).readline))
+        self.tokens.replace_text(self._pretty(tokens))
 
-        self.tokens.replace_text(pretty(tokens))
-        self.ast.replace_text(pretty(ast.dump(ast.parse(src))))
-        self.opt_ast.replace_text(pretty(ast.dump(ast.parse(src, optimize=1))))
+    def refresh_ast(self):
+        src = self.source.getvalue()
+        self.ast.replace_text(
+            self._pretty(ast.dump(ast.parse(src))))
+
+    def refresh_optimized_ast(self):
+        src = self.source.getvalue()
+        self.opt_ast.replace_text(
+            self._pretty(ast.dump(ast.parse(src, optimize=1))))
 
     def close(self):
         self.destroy()
